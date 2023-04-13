7f865d2ac5af031f39a4c026f575f4aba3fcdd28
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-14 00:07:01 +0200

    authentication service

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import {HttpClient, HttpHeaders} from "@angular/common/http";
 import { map, Observable } from "rxjs";
 const PROTOCOL = "http";
 const PORT = 3000;
@@ -7,15 +7,22 @@ const PORT = 3000;
 export class RestDataSource {
   baseUrl: string;
   auth_token?: string;
+  httpOptions: object;
   constructor(private http: HttpClient) {
     this.baseUrl = `${PROTOCOL}://${location.hostname}:${PORT}/`;
+    this.httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
   }
   authenticate(user: string, pass: string): Observable<boolean> {
-    return this.http.post<any>(this.baseUrl + "authentication/sign-in", {
-      name: user, password: pass
-    }).pipe(map(response => {
-      this.auth_token = response.success ? response.accessToken : null;
-      return response.success;
+    return this.http
+      .post<any>(this.baseUrl + "authentication/sign-in", {
+      email: user, password: pass
+    }, this.httpOptions).pipe(map(response => {
+      this.auth_token = response.accessToken ? response.accessToken : null;
+      return true;
     }));
   }
 }
