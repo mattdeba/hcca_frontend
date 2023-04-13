3962adf2215b07bf5d16912bfad48cfaa4638f99
matthieu debarge <debarge.matthieu@gmail.com>
2023-04-13 19:59:49 +0200

    add hcca component

---
+++ src/app/hcca/hcca.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+import { RouterModule } from "@angular/router";
+import { AuthComponent } from "./auth.component";
+import { HccaComponent } from "./hcca.component";
+let routing = RouterModule.forChild([
+  { path: "auth", component: AuthComponent },
+  { path: "main", component: HccaComponent },
+  { path: "**", redirectTo: "auth" }
+]);
+@NgModule({
+  imports: [CommonModule, FormsModule, routing],
+  declarations: [AuthComponent, HccaComponent]
+})
+export class HccaModule { }
